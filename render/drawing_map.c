1a3af3f473f5e0151ee7c5faae2ed81f668d4ba0
Ayoub Adouay <ayadouay@c4r6p8.1337.ma>
2025-07-19 14:42:36 +0100

    drawing in progress...

---
+++ render/drawing_map.c
@@ -0,0 +1,44 @@
+#include "includes/cub3d.h"
+
+//hadchy ba9i khdam alih mat9isoch !! 
+// void	my_mlx_pixel_put(t_img *data, int x, int y, int color)
+// {
+// 	char	*dst;
+
+// 	dst = data->addr + (y * data->line_len + x * (data->bpp / 8));
+// 	*(unsigned int*)dst = color;
+// }
+
+// void    draw_square(t_game *data, int y, int x, int color)
+// {
+    
+// }
+// void    draw_cercle()
+// {
+
+// }
+// void    drawing_map(t_game *data)
+// {
+//     int y;
+//     int x;
+//     y = 0;
+
+//     while (y < data->map.height)
+//     {
+//         x = 0;
+//         while (x < data->map.width)
+//         {
+//             if(data->map.grid[y][x] == '1')
+//                 draw_square();
+//             else if(data->map.grid[y][x] == '0')
+//                 draw_square();
+//             else if(data->map.grid[y][x] == 'N')
+//                 draw_cercle();
+//             else if(data->map.grid[y][x] == ' ')
+//                 draw_square();
+//             x++;
+//         }
+//         y++;
+//     }
+//     return ;
+// }
